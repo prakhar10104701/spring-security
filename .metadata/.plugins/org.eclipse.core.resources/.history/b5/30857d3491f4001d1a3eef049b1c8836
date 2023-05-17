d1336cbcb839072e8b37a00d6fe7da74fe3b7fc9
prakhar10104701 <prakhar10104701@gmail.com>
2023-05-17 16:24:05 +0530

    commiting spring security POC file'

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/b5/30857d3491f4001d1a3eef049b1c8836
@@ -0,0 +1,9 @@
+package com.spring.security.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.security.models.User;
+
+public interface UserRepository extends JpaRepository<User, String>{
+
+}
---
+++ SpringSecurityDemo/src/main/java/com/spring/security/models/User.java
@@ -0,0 +1,24 @@
+package com.spring.security.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+	@Id
+	String username;
+	String password;
+	String email;
+	String role;
+	
+	
+}
